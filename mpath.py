bbb38820c7dd7ed133f53fc0e0613f4d1232f0ea
gfixler <gary@garyfixler.com>
2020-05-21 22:49:23 -0700

    Add path table building
    This was called flatten for a moment there, and that's the idea. This
    new .buildPathTable method walks the tree, and creates a single-level
    dict from the names tagging each level, each pointing to the fully
    resolved path. This allows the main use of this module's currently only
    class, which is to tag interesting parts of the project's file system
    for easy retrieval, all throughout tools in Maya.

--- mpath.py
+++ mpath.py
@@ -1,8 +1,19 @@
+import os
+
+
 class MPaths (object):
 
     def __init__ (self, pathTree):
-        self.pathsTree = pathTree
+        self.pathTree = pathTree
+        self.pathTable = {}
+        self.buildPathTable(pathTree)
+
+    def buildPathTable (self, tree, prefix=""):
+        for n, r, ps in tree:
+            pathSoFar = os.path.join(prefix, r)
+            self.pathTable[n] = pathSoFar
+            self.buildPathTable(ps, prefix=pathSoFar)
 
     def getRoots (self):
-        return map(lambda (_, r, __): r, self.pathsTree)
+        return map(lambda (_, r, __): r, self.pathTree)
 
--- test/test_mpath.py
+++ test/test_mpath.py
@@ -44,3 +44,10 @@ class Test_MPaths (unittest.TestCase):
         mpaths = mpath.MPaths(winA)
         self.assertEquals(mpaths.getRoots(), ["C:/game", "Z:/game"])
 
+    def test_canGetPathsByName (self):
+        mpaths = mpath.MPaths(linA)
+        self.assertEquals(mpaths.pathTable["root"], "/home/gfixler/proj")
+        self.assertEquals(mpaths.pathTable["charSrc"], "/home/gfixler/proj/art/chars")
+        self.assertEquals(mpaths.pathTable["charExp"], "/home/gfixler/proj/assets/chars")
+        self.assertEquals(set(mpaths.pathTable.keys()), set(["root", "charSrc", "charExp"]))
+
