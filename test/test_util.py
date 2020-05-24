3a479b92cabd0527cd5a9144f5a58f63e48f8a83
gfixler <gary@garyfixler.com>
2020-05-23 21:55:08 -0700

    add parseHier to util.py
    I'm not entirely in love with the flags here; they feel more about the
    needs of where I'll be using this than about the actual function itself.

--- test/test_util.py
+++ test/test_util.py
@@ -22,3 +22,63 @@ class Test_parseIndent (unittest.TestCase):
     def test_level15 (self):
         self.assertEquals(util.parseIndent("               fifteen"), (15, "fifteen"))
 
+
+class Test_parseHier (unittest.TestCase):
+
+    def test_emptystring (self):
+        # note: python's str.splitlines turns "" into []
+        self.assertEquals(util.parseHier(""), [])
+
+    def test_oneLine (self):
+        self.assertEquals(util.parseHier("This is a test."), [(0, "This is a test.")])
+
+    def test_twoLinesOneIndent (self):
+        self.assertEquals(util.parseHier("This is\n    a test."), [(0, "This is"), (1, "a test.")])
+
+    def test_twoLinesOneIndentWindowsNewlines (self):
+        self.assertEquals(util.parseHier("This is\r\n    a test."), [(0, "This is"), (1, "a test.")])
+
+    def test_multilineString_defaults (self):
+        example = """
+        This is
+            a test
+            of
+                a multiline
+        string
+        """
+        expected = [(0, "This is"), (1, "a test"), (1, "of"), (2, "a multiline"), (0, "string")]
+        self.assertEquals(util.parseHier(example), expected)
+
+    def test_multilineString_nonNormalized (self):
+        example = """
+        This is
+            a test
+            of
+                a multiline
+        string
+        """
+        expected = [(8, "This is"), (12, "a test"), (12, "of"), (16, "a multiline"), (8, "string")]
+        self.assertEquals(util.parseHier(example, normalized=False), expected)
+
+    def test_multilineString_filterEmpty (self):
+        example = """
+        This is
+            a test
+            of
+                a multiline
+        string
+        """
+        expected = [(0, ""), (1, "This is"), (2, "a test"), (2, "of"), (3, "a multiline"), (1, "string"), (1, "")]
+        self.assertEquals(util.parseHier(example, filterEmpty=False), expected)
+
+    def test_multilineString_nonNormalizedFilterEmpty (self):
+        example = """
+        This is
+            a test
+            of
+                a multiline
+        string
+        """
+        expected = [(0, ""), (8, "This is"), (12, "a test"), (12, "of"), (16, "a multiline"), (8, "string"), (8, "")]
+        self.assertEquals(util.parseHier(example, normalized=False, filterEmpty=False), expected)
+
--- util.py
+++ util.py
@@ -1,6 +1,13 @@
 import re
 
 
+ident = lambda x: x
+fst = lambda (x, _): x
+snd = lambda (_, y): y
+onFst = lambda f: lambda (x, y): (f(x), y)
+swap = lambda (x, y): (y, x)
+
+
 def parseIndent (line):
     match = re.match("^(\s+)", line)
     if match:
@@ -9,4 +16,21 @@ def parseIndent (line):
         return (indentLen, line[indentLen:])
     else:
         return (0, line)
+
+def parseHier (string, normalized=True, filterEmpty=True):
+    rawlines = string.splitlines()
+    if filterEmpty:
+        lines = filter(ident, rawlines)
+    else:
+        lines = rawlines
+    rawindents = map(parseIndent, lines)
+    if filterEmpty:
+        indents = filter(snd, rawindents)
+    else:
+        indents = rawindents
+    if normalized:
+        levels = sorted(list(set(map(fst, indents))))
+        normMap = dict(map(swap, enumerate(levels)))
+        return map(onFst(lambda x: normMap[x]), indents)
+    return indents
 
