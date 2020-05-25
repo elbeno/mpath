9fef8f7b1b019be7846bed6e7029318db4a2bfe4
gfixler <gary@garyfixler.com>
2020-05-25 15:14:16 -0700

    Test initing an Mpath with a parsedLayout

--- test/test_mpath.py
+++ test/test_mpath.py
@@ -82,3 +82,11 @@ class Test_MPaths (unittest.TestCase):
         self.assertEquals(mpaths.pathTable["charExp"], "/home/gfixler/proj/assets/chars")
         self.assertEquals(set(mpaths.pathTable.keys()), set(["root", "charSrc", "charExp"]))
 
+    def test_canMakeFromParsedLayout (self):
+        # this one is more of an integration test
+        mpaths = mpath.MPaths(mpath.parseLayout(winALayout))
+        self.assertEquals(mpaths.pathTable["srcRoot"], "C:/game")
+        self.assertEquals(mpaths.pathTable["charSrc"], "C:/game/art/chars")
+        self.assertEquals(mpaths.pathTable["itemsDoc"], "C:/game/art/chars/Docs/items.json")
+        self.assertEquals(set(mpaths.pathTable.keys()), set(["srcRoot", "charSrc", "docs", "itemsDoc", "expRoot", "charExp"]))
+
