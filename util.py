d7a72c95138f3a83d511640cc5d65676b0b10b6d
Ben Deane <ben@elbeno.com>
2020-05-31 16:38:19 -0600

    wip

--- util.py
+++ util.py
@@ -10,38 +10,38 @@ swap = lambda (x, y): (y, x)
 
 
 def parseIndent (line):
-    match = re.match("^(\s+)", line)
-    if match:
-        indent = match.group()
-        indentLen = len(indent)
-        return (indentLen, line[indentLen:])
-    else:
-        return (0, line)
+    stripped = line.lstrip()
+    indent = len(line) - len(stripped)
+    return (indent, stripped)
 
-def parseHierStr (string, normalize=True, filterEmpty=True):
+def normalizeIndents(lines, index=0, curIndent=0, level=0):
+    normalized = []
+    while index < len(lines):
+        (indent, line) = lines[index]
+        if indent > curIndent:
+            raise RuntimeError, "outdent to non-existing indent level in line " + str(index) + ": " + str((indent, line))
+        elif indent < curIndent:
+            return (normalized, index)
+
+        normalized.append((level, line))
+        (children, index) = normalizeSubtree(lines, index+1, curIndent, level+1)
+        normalized.extend(children)
+
+    return (normalized, index)
+
+def normalizeSubtree (lines, index=0, curIndent=-1, level=0):
+    if index < len(lines):
+        (nextIndent, _) = lines[index]
+        if nextIndent > curIndent:
+            return normalizeIndents(lines, index, nextIndent, level)
+    return ([], index)
+
+def parseHierStr(string, normalize=True, filterEmpty=True):
     lines = string.splitlines()
     withIndents = map(parseIndent, lines)
     p = snd if filterEmpty else true
     filtered = filter(p, withIndents)
-
     if not normalize:
         return filtered
-
-    curIndent = -1
-    indentStack = []
-    normLevel = -1
-    normalized = []
-    for linenum, (indent, part) in enumerate(filtered):
-        if indent > curIndent:
-            indentStack.append(curIndent)
-            curIndent = indent
-            normLevel = normLevel + 1
-        elif indent < curIndent:
-            while curIndent > indent:
-                if indent not in indentStack:
-                    raise RuntimeError, "outdent to non-existing indent level in line " + str(linenum + 1) + ": " + str((indent, part))
-                curIndent = indentStack.pop()
-                normLevel = normLevel - 1
-        normalized.append((normLevel, part))
-    return normalized
-
+    n, _ = normalizeSubtree(filtered)
+    return n
